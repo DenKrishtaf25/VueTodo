79245bf627dc97f9e0b6699b3be8197dd88a4e46
DenKrishtaf25 <dkrishtaf98@gmail.com>
2021-07-18 01:11:24 +0300

    wer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h1>Todo application</h1>
+    <AddTodo 
+      @add-todo='addTodo'
+    />
     <hr>
     <TodoList 
       v-bind:todos="todos"
@@ -11,6 +14,7 @@
 
 <script>
 import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
 
 export default {
   name: 'App',
@@ -26,10 +30,13 @@ export default {
   methods: {
     removeTodo(id){
       this.todos = this.todos.filter(t=> t.id !==id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
     }
   },
   components: {
-    TodoList
+    TodoList, AddTodo
   }
 }
 </script>
