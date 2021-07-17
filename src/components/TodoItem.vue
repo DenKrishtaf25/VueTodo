9787a7af57dc7382af72a8c42627b7bbfa3abed6
DenKrishtaf25 <dkrishtaf98@gmail.com>
2021-07-17 21:05:04 +0300

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <span v-bind:class='{done: todo.completed}'>
+      <input type="checkbox" v-on:change='todo.completed = !todo.completed'>
+      <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+    </span>
+    <button 
+        class="rm" 
+        v-on:click="$emit('remove-todo', todo.id)"
+    >&times;</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 2rem;
+  margin-bottom: 1rem;
+}
+.rm{
+    background: red;
+    border: none;
+    color: #fff;
+    border-radius: 25%;
+    font-weight: bold;
+    cursor: pointer;
+}
+input{
+    margin-right: 1rem;
+}
+.done{
+    text-decoration: line-through;
+}
+</style>
