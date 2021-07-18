be56ec5df24763c6f1cd0fd28026a7495b26bb8e
DenKrishtaf25 <dkrishtaf98@gmail.com>
2021-07-18 14:54:18 +0300

    sdf

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,7 +3,7 @@
     <span v-bind:class='{done: todo.completed}'>
       <input type="checkbox" v-on:change='todo.completed = !todo.completed'>
       <strong>{{ index + 1 }}</strong>
-      {{ todo.title }}
+      {{ todo.title | uppercase}}
     </span>
     <button 
         class="rm" 
@@ -21,6 +21,11 @@ export default {
     },
     index: Number
   },
+  filters: {
+    uppercase(value){
+      return value.toUpperCase();
+    }
+  }
 };
 </script>
 
